fb56de28af2f688d57ec63a4c724a5bab9cdea64
j-ami-l <mdhasanjamil0001@gmail.com>
2025-09-26 14:21:15 +0600

    fixed

--- src/Pages/ERROR/ERROR.jsx
+++ src/Pages/ERROR/ERROR.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router';
-import ERROR_Animation from '../../Components/ERROR_Animation/ERROR_Animation';
 
 const ERROR = () => {
     return (
         <div className=''>
             <div className='lg:w-[60%] mx-auto mt-20'>
-                <ERROR_Animation></ERROR_Animation>
+                <h1>Errorrrrrrr</h1>
             </div>
             <div className='flex items-center justify-center mt-5'>
                 <Link to={'/'}><button className='cursor-pointer px-6 py-2 bg-green-600  rounded font-semibold shadow-md transition text-white'>Back To Home</button></Link>
--- src/components/ERROR_Animation/ERROR_Animation.jsx
+++
@@ -1,7 +0,0 @@
-import Lottie from "lottie-react";
-
-const ERROR_Animation = () => {
-  return <h1>Error</h1>
-};
-
-export default ERROR_Animation;
